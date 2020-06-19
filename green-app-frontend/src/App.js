df1ae5db298bdf6176f471c34f1c766a59dcd28b
Philip Wee <philip.andrewweedewang@gmail.com>
2020-06-19 09:56:13 +0800

    Navbar and linking Done

--- green-app-frontend/src/App.js
+++ green-app-frontend/src/App.js
@@ -1,34 +1,39 @@
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from './logo.svg';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './App.css';
 
 //Component Imports
-import AccountabilityBuddyChat from "./components/accountability-buddy-chat.component";
-import SimilarActivitiesFinder from "./components/similar-activities-finder.component";
-import CarbonEmissionCalculator from "./components/carbon-emission-calculator.component";
-import GoalSetterWithBuddy from "./components/goal-setter-with-buddy.component";
-import ActivityGeneration from "./components/activity-generation.component";
-import CertificateGeneration from "./components/certificate-generation.component";
-import CommunityPage from "./components/community-page.component";
-import LandingPage from "./components/landing-page.component";
+import AccountabilityBuddyChat from "./components/pages/accountability-buddy-chat.component";
+import SimilarActivitiesFinder from "./components/pages/similar-activities-finder.component";
+import CarbonEmissionCalculator from "./components/pages/carbon-emission-calculator.component";
+import GoalSetterWithBuddy from "./components/pages/goal-setter-with-buddy.component";
+import ActivityGeneration from "./components/pages/activity-generation.component";
+import CertificateGeneration from "./components/pages/certificate-generation.component";
+import CommunityPage from "./components/pages/community-page.component";
+import LandingPage from "./components/pages/landing-page.component";
+
+import Navbar from "./components/navbars/navbar.component";
 
 
 class App extends Component {
   render() {
     return (
+
       <Router>
-        
-        <Route path="/accountability-buddy-chat" exact component={AccountabilityBuddyChat} />
-        <Route path="/similar-activities-finder" exact component={SimilarActivitiesFinder} />
-        <Route path="/carbon-emission-calculator" exact component={CarbonEmissionCalculator} />
-        <Route path="/goal-setter-with-buddy" exact component={GoalSetterWithBuddy} />
-        <Route path="/activity-generation" exact component={ActivityGeneration} />
-        <Route path="/certificate-generation" exact component={CertificateGeneration} />
-        <Route path="/community-page" exact component={CommunityPage} />
-        <Route path="/accountability-buddy-chat" exact component={AccountabilityBuddyChat} />
-        <Route path="/" exact component={LandingPage} />
+        <Navbar></Navbar>
+        <Switch>
+          <Route path="/accountability-buddy-chat" exact component={AccountabilityBuddyChat} />
+          <Route path="/similar-activities-finder" exact component={SimilarActivitiesFinder} />
+          <Route path="/carbon-emission-calculator" exact component={CarbonEmissionCalculator} />
+          <Route path="/goal-setter-with-buddy" exact component={GoalSetterWithBuddy} />
+          <Route path="/activity-generation" exact component={ActivityGeneration} />
+          <Route path="/certificate-generation" exact component={CertificateGeneration} />
+          <Route path="/community-page" exact component={CommunityPage} />
+          <Route path="/" exact component={LandingPage} />
+        </Switch>
+
       </Router>
     );
   }
---
+++ green-app-frontend/src/components/navbars/navbar.component.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+  Button
+} from 'reactstrap';
+
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" dark expand="md">
+        <NavbarBrand href="/">Green App</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/carbon-emission-calculator/">Carbon Emission Calculator</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                My Environment
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem href='/activity-generation'>
+                  My Activities
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem href='/goal-setter-with-buddy'>
+                  My Goals
+                </DropdownItem>
+
+                <DropdownItem href='/certificate-generation'>
+                  My Certificates
+                </DropdownItem>
+
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Our Environment
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem href='/community-page'>
+                  Our Community
+                </DropdownItem>
+                {/* <DropdownItem href='/community-page'>
+                Our Shared Activities
+                </DropdownItem> */}
+                <DropdownItem divider />
+                <DropdownItem href='/accountability-buddy-chat'>
+                  Buddy Chat
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+
+          <Button className="mr-1" color="primary">Sign Up</Button>{' '}
+          <Button color="primary">Log In</Button>{' '}
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Example;

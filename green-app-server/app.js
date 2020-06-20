d50414f1286540c80fd7eccdb10c9f09e2d8a1f6
lowen <lowenhere@gmail.com>
2020-06-21 02:16:35 +0800

    milestones page loads from backend

--- green-app-server/app.js
+++ green-app-server/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 
 // Constants
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 const HOST = process.env.HOST || 'localhost';
 
 // App
@@ -9,6 +10,12 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+app.use(cors({
+  origin:['http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use('/challenges', require('./src/challenges'));
 app.use('/rewards', require('./src/rewards'));
